434a2504e4622e79017135ca014c9692e2e1aaa8
joseildo filho <joseildofilho@yahoo.com.br>
2019-12-16 11:01:56 -0300

    updating testes

--- test_experiment.jl
+++ test_experiment.jl
@@ -8,37 +8,29 @@ basic_state = []
 
 @test_throws ArgumentError get_state(basic_state)
 
-basic_state = [1e6, 1e6]
-@test_throws ArgumentError get_state(basic_state, n=0)
-
-basic_state = [1e6, 1e6]
-@test_throws ArgumentError get_state(basic_state, n=-1)
-
 basic_state = [-1e6, 1e6]
-@test_throws ArgumentError get_state(basic_state, n=-1)
+@test_throws ArgumentError get_state(basic_state)
 
 basic_state = [1e6, -1e6]
-@test_throws ArgumentError get_state(basic_state, n=-1)
+@test_throws ArgumentError get_state(basic_state)
 
 basic_state = [-1e6, -1e6]
-@test_throws ArgumentError get_state(basic_state, n=-1)
+@test_throws ArgumentError get_state(basic_state)
 
-basic_state = [1e6, 1e6]
-@test_throws ArgumentError get_state(basic_state, factor=0.0)
-
-basic_state = [1e6, 1e6]
+basic_state = [1, 1]
 @test get_state(basic_state) == (1, 1)
 
-basic_state = (*).(2, basic_state)
+basic_state = [1e6, 1e6]
+@test get_state(basic_state) == (10, 10)
+
+basic_state = [200000, 200000]
 @test get_state(basic_state) == (2, 2)
 
 basic_state = (*).(5, basic_state)
 @test get_state(basic_state) == (10, 10)
 
-basic_state = (*).(5, basic_state)
-@test get_state(basic_state) == (10, 10)
 
 basic_state = [1e6, 1e6, 1e5]
-@test get_state(basic_state) == (1, 1, 0)
+@test get_state(basic_state) == (10, 10, 1)
 
 ############### TESTING GET_STATE ###############
